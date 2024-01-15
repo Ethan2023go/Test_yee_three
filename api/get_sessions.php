4a74153be84d1f2d8368a5a222f40004ca8ffb1a
Ethan <easonbest123@gmail.com>
2024-01-15 15:18:33 +0800

    場次功能(將日期時間限制加入)  完成

--- api/get_sessions.php
+++ api/get_sessions.php
@@ -2,9 +2,10 @@
 $movie=$_GET['movie'];
 $date=$_GET['date'];
 $H=date("G");
-$start=($H<14)?1:7-ceil((24-$H)/2);
+$start=($H>=14 && $date==date("Y-m-d"))?7-ceil((24-$H)/2):1;
 
-for($i=1;$i<=5;$i++){
+for($i=$start;$i<=5;$i++){
+    // 1. 去資料表撈出電影日期
     echo "<option value='{$sess[$i]}'>{$sess[$i]} 剩餘座位 20</option>";
 }
 
