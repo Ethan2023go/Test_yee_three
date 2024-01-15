20f198797691a9ba4ffa84705a7c2ae7a139ff43
Ethan <easonbest123@gmail.com>
2024-01-15 16:43:50 +0800

    剩餘座位 功能 完成

--- front/order.php
+++ front/order.php
@@ -1,3 +1,4 @@
+<div id='select'>
 <h3 class="ct">線上訂票</h3>
 <div class="order">
     <div>
@@ -14,28 +15,54 @@
         <select name="session" id="session"></select>
     </div>
     <div>
-        <button>確定</button>
+        <button onclick="$('#select').hide();$('#booking').show()">確定</button>
         <button>重置</button>
     </div>
 </div>
+</div>
+
+<style>
+ #room{
+    background-image: url('./icon/03D04.png');
+    background-position: center;
+    background-repeat: none;
+    width:540px;
+    height:370px;
+    margin:auto;
+ }   
+</style>
+<div id="booking" style='display:none'>
+<div id="room"></div>
+<div id="info">
+
+<button onclick="$('#select').show();$('#booking').hide()">上一步</button>
+<button>訂購</button>
+</div>
+</div>
+
+
+
+
 <script>
 let url=new URL(window.location.href)
+
 getMovies();
 
 $("#movie").on("change",function(){
     getDates($("#movie").val())
 })
+
 $("#date").on("change",function(){
-    getSessions($("#movie").val(),$('#date').val())
+    getSessions($("#movie").val(),$("#date").val())
 })
 
 function getMovies(){
     $.get("./api/get_movies.php",(movies)=>{
-            $("#movie").html(movies);
-            if(url.searchParams.has('id')){
+        $("#movie").html(movies);
+        if(url.searchParams.has('id')){
            $(`#movie option[value='${url.searchParams.get('id')}']`).prop('selected',true);
         }
-            getDates($("#movie").val())
+        getDates($("#movie").val())
     })
 }
 function getDates(id){
