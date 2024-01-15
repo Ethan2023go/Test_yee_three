240776c76523bb09467323fdd44da9bb7606bf05
Ethan <easonbest123@gmail.com>
2024-01-15 13:19:53 +0800

    建構前端連動選單的三個函式架構

---
+++ api/get_dates.php
@@ -0,0 +1,16 @@
+<?php include_once 'db.php';
+$movie=$_GET['id'];
+$ondate=$Moive->find($movie)['ondate'];
+$date=date("Y-m-d");
+// ondate     today  3
+// ondate +1  today  2
+// ondate +2  today  1
+for($i=0;$i<3;$i++){
+    $date=strtotime("+$i days",strtotime($ondate));
+    if($date>=strtotime($today)){
+        $str=date("Y-m-d",$date);
+        echo "<option value='{$str}'> $str </option>";
+    }
+}
+
+?>
